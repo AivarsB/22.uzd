c2476c1cde7ab5c671e89e1a688a3827b44447cd
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 15:42:25 +0200

    Add project files.

---
+++ 22.uzd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _22.uzd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeekDayEnum x = WeekDayEnum.Piektdiena;
+            Console.WriteLine(x);
+
+        }
+    }
+}
+
+
+// Izveidot console aplikāciju, kurā ir definēts nedēļas dienu enum (Enumerātors). 
+// Deklarēt programmā jaunu mainīgo, kā data tipu tam norādot izveidoto enum un piešķirt vērtību "Piektdiena".
---
+++ 22.uzd/WeekDayEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22.uzd
+{
+    public enum WeekDayEnum
+    {
+        Pirmdiena,
+        Otrdiena,
+        Tresdiena,
+        Ceturtdiena,
+        Piektdiena,
+        Sestdiena,
+        Svetdiena
+    }
+}
